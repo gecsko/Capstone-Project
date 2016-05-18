13df341f6a2d30ec391abbb2f229d3a8cb50c481
gecsko <gecsko@gmail.com>
2016-05-19 01:52:23 +0800

    ad google map
    add time
    
    all fragment connected

--- MiniDiary/app/src/main/java/com/example/kathy/minidiary/MainActivity.java
+++ MiniDiary/app/src/main/java/com/example/kathy/minidiary/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.kathy.minidiary;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
+    private boolean twoPaneMode = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,48 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.framelayout_main, mainFragment);
             ft.commit();
+        }
+
+    }
+
+    public void openDetailFragment(HashMap<String, String> selectedDiary) {
+        //String selectedArtistId = (String) selected.get("id");
+        if (twoPaneMode) {
+            //Intent intent = new Intent(this, MainActivity.class).putExtra(Intent.EXTRA_TEXT, selectedArtistId);
+
+            Bundle data = new Bundle();
+            //data.putString("selectedArtistId", selectedArtistId);
+
+            DetailFragment detailFragment = new DetailFragment();
+            detailFragment.setArguments(data);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.framelayout_detail, detailFragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class).putExtra("selectedDiary", selectedDiary);
+            startActivity(intent);
+        }
+
+    }
+
+    public void openAddDiaryFragment() {
+        //String selectedArtistId = (String) selected.get("id");
+        if (twoPaneMode) {
+            //Intent intent = new Intent(this, MainActivity.class).putExtra(Intent.EXTRA_TEXT, selectedArtistId);
+
+            /*Bundle data = new Bundle();
+            //data.putString("selectedArtistId", selectedArtistId);
+
+            DetailFragment detailFragment = new DetailFragment();
+            detailFragment.setArguments(data);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.framelayout_detail, detailFragment)
+                    .commit();*/
+        } else {
+            Intent intent = new Intent(this, AddDiaryActivity.class);
+            startActivity(intent);
         }
 
     }
--- MiniDiary/app/src/main/java/com/example/kathy/minidiary/MainFragment.java
+++ MiniDiary/app/src/main/java/com/example/kathy/minidiary/MainFragment.java
@@ -1,17 +1,21 @@
 package com.example.kathy.minidiary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ShareCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
 
 public class MainFragment extends Fragment {
@@ -36,16 +40,16 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        ArrayList<Hashtable<String, String>> items = new ArrayList<Hashtable<String, String>>();
+        ArrayList<HashMap<String, String>> items = new ArrayList<HashMap<String, String>>();
 
-        Hashtable<String, String> e1 = new Hashtable<String, String>();
+        HashMap<String, String> e1 = new HashMap<String, String>();
         e1.put("Title", "ABC");
         e1.put("Date", "13:15 02052016");
         e1.put("Content", "Happy");
 
         items.add(e1);
 
-        Hashtable<String, String> e2 = new Hashtable<String, String>();
+        HashMap<String, String> e2 = new HashMap<String, String>();
         e2.put("Title", "Shopping");
         e2.put("Date", "14:00 03052016");
         e2.put("Content", "Sad");
@@ -60,6 +64,23 @@ public class MainFragment extends Fragment {
         simpleAdapter = new SimpleAdapter(this.getActivity(), items, R.layout.fragment_listview_item, new String[]{"Title", "Date", "Content"},
                 new int[]{R.id.listview_title, R.id.listview_item_data_time, R.id.detail_location});
         listView.setAdapter(simpleAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                HashMap<String, String> selectedDiary = (HashMap<String, String>) simpleAdapter.getItem(position);
+
+                ((MainActivity)getActivity()).openDetailFragment(selectedDiary);
+            }
+        });
+
+        rootView.findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).openAddDiaryFragment();
+            }
+        });
         //listView.setAdapter(mUserAdapter);
         //listView.setOnItemClickListener(this);
         return rootView;
